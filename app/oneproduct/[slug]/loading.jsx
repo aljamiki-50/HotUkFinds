e0820911832b2c3eb5b94be5a5ec5d05937f3867
aljamiki-50 <almazin77@yahoo.com>
2024-03-19 09:54:39 +0000

    add metatag to the page.jsx

--- app/oneproduct/[slug]/loading.jsx
+++ app/oneproduct/[slug]/loading.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Loading = () => {
   return (
     <div className="min-h-screen bg-yellow-900 text-center">
-        <span className="visually-hidden">Loading...</span>
+      <span className="visually-hidden">Loading...</span>
       <h1 className="mt-5">Loading...</h1>
       <p className="lead">Please wait while we load the content...</p>
     </div>
--- app/page.js
+++ app/page.js
@@ -2,6 +2,17 @@ import AllProducts from "@/components/AllProducts/AllProducts";
 import Banner from "@/components/Banner/Banner";
 import Image from "next/image";
 
+export const metadata = {
+  robots: { index: true, follow: true },
+  title: " ✅ Home",
+  // in case for absoulte Declaration at the tab it self
+  // title:{
+  //   absoulte: " ✅ AboutUs",
+  // },
+
+  ogImage: "/opengraph-image.jpg",
+};
+
 export default function Home() {
   return (
     <main className=" h-[10%] items-center justify-between overflow-hidden ">
