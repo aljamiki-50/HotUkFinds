e6a9f003cc6e70084d7bcf57ba0e505148d584b3
aljamiki-50 <almazin77@yahoo.com>
2024-03-20 13:44:43 +0000

    removing the redundancy in sitemap that was confusing google to index some pages

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,16 +3,30 @@ import { GetData } from "@/Helpers/allProductInMainPage/GetData";
 export default async function sitemap() {
   const products = await GetData();
 
-  const postentries = products.map((product) => {
-    return {
-      // url: `${process.env.NEXTAUTH_URL}category/${encodeURIComponent(product?.categoryName)}`,
-      url: `${process.env.NEXTAUTH_URL}category/${product?.categoryName}`,
-      url2: `${process.env.NEXTAUTH_URL}category/${product?.slug}`,
-      lastmod: product?.lastmod,
-      changefreq: "daily",
-      priority: 0.7,
-    };
-  });
+  const uniqueCategories = new Set(
+    products.map((product) => product?.categoryName)
+  );
+
+  // Generate URLs for unique categories
+  const categoryEntries = Array.from(uniqueCategories).map((categoryName) => ({
+    url: `${process.env.NEXTAUTH_URL}category/${encodeURIComponent(
+      categoryName
+    )}`,
+    lastmod: "2022-01-01", // Set last modification date accordingly
+    changefreq: "daily",
+    priority: 0.7,
+  }));
+
+  // const postentries = products.map((product) => {
+  //   return {
+  //     // url: `${process.env.NEXTAUTH_URL}category/${encodeURIComponent(product?.categoryName)}`,
+  //     url: `${process.env.NEXTAUTH_URL}category/${product?.categoryName}`,
+  //     url2: `${process.env.NEXTAUTH_URL}category/${product?.slug}`,
+  //     lastmod: product?.lastmod,
+  //     changefreq: "daily",
+  //     priority: 0.7,
+  //   };
+  // });
 
   const another = products.map((oneproduct) => {
     return {
@@ -42,8 +56,10 @@ export default async function sitemap() {
       changefreq: "monthly",
       priority: 0.8,
     },
-    ...postentries,
+    // ...postentries,
+    ...categoryEntries,
     ...another,
+   
   ];
 
   return sitemap;
